248812a6a28cc8b8e6901e188b8c95104b31c0e4
Lyilan <66785329+lyilan@users.noreply.github.com>
2023-07-24 15:54:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '莹莹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ou8C96QB8TohXDlp2iIsFOkSYeT0',
+      id: 'ou8C96ScGQ2ejvgZkl6UikAV5CTg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Ng6RGsW-WIlTSYjwIsSOaJMfmwpukcZWG0Ipz04FSWE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
